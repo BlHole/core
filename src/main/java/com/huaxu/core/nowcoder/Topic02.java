28f0aeb20e312a5f141df47e55e92a478e5dd207
huaxu <tshy0425@hotmail.com>
2020-01-16 14:33:12 +0800

    nowcoder solution.java topic02

---
+++ src/main/java/com/huaxu/core/nowcoder/Topic02.java
@@ -0,0 +1,14 @@
+package com.huaxu.core.nowcoder;
+
+/**
+ * <p>project: core</p>
+ * <p>description: </p>
+ * <p>create: 2020/01/16 14:31</p>
+ * <p>author：huaxu</p>
+ */
+public class Topic02 {
+
+    public String replaceSpace(StringBuffer str) {
+        return str.toString().replace(" ", "%20");
+    }
+}
