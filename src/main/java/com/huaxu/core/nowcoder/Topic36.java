511e9c5ed7cc2d30787a6ebf3639d9717bff5b79
huaxu <tshy0425@hotmail.com>
2020-01-19 17:56:30 +0800

    nowcoder solution.java topic36

---
+++ src/main/java/com/huaxu/core/nowcoder/Topic36.java
@@ -0,0 +1,26 @@
+package com.huaxu.core.nowcoder;
+
+import com.huaxu.core.leetcode.basic.TreeNode;
+
+import java.util.LinkedList;
+
+/**
+ * <p>project: core</p>
+ * <p>description: </p>
+ * <p>create: 2020/01/19 17:43</p>
+ * <p>author：huaxu</p>
+ *
+ * 二叉树的深度
+ *
+ * 题目描述
+ * 输入一棵二叉树，求该树的深度。从根结点到叶结点依次经过的结点（含根、叶结点）形成树的一条路径，最长路径的长度为树的深度。
+ *
+ * links:
+ */
+public class Topic36 {
+
+    public int TreeDepth(TreeNode root) {
+        if (root == null) return 0;
+        return Math.max(TreeDepth(root.left) + 1, TreeDepth(root.right) + 1);
+    }
+}
