3cd2c29151ed3dc2e3a824765407dd1ac2af8c99
huaxu <tshy0425@hotmail.com>
2020-02-02 17:53:23 +0800

    nowcoder solution.java topic49

--- src/main/java/com/huaxu/core/nowcoder/Topic49.java
+++ src/main/java/com/huaxu/core/nowcoder/Topic49.java
@@ -15,21 +15,42 @@ import java.util.LinkedList;
  * 而'*'表示它前面的字符可以出现任意次（包含0次）。 在本题中，匹配是指字符串的所有字符匹配整个模式。
  * 例如，字符串"aaa"与模式"a.a"和"ab*ac*a"匹配，但是与"aa.a"和"ab*a"均不匹配
  *
- * links:
+ * links: https://www.nowcoder.com/questionTerminal/45327ae22b7b413ea21df13ee7d6429c
  */
 public class Topic49 {
 
     public boolean match(char[] str, char[] pattern) {
-        int i = 0, j = 0, lStr = str.length, pStr= pattern.length;
-        while (i < lStr && j < pStr) {
-            if ('.' == pattern[j] || str[i] == pattern[j]) {
-                i ++; j++;
-                continue;
-            }
-            if (str[i] != pattern[j] && j + 1 < pStr) {
+        if (str == null || pattern == null) {
+            return false;
+        }
+        int strIndex = 0;
+        int patternIndex = 0;
+        return matchCore(str, strIndex, pattern, patternIndex);
+    }
 
+    public boolean matchCore(char[] str, int strIndex, char[] pattern, int patternIndex) {
+        //有效性检验：str到尾，pattern到尾，匹配成功
+        if (strIndex == str.length && patternIndex == pattern.length) {
+            return true;
+        }
+        //pattern先到尾，匹配失败
+        if (strIndex != str.length && patternIndex == pattern.length) {
+            return false;
+        }
+        //模式第2个是*，且字符串第1个跟模式第1个匹配,分3种匹配模式；如不匹配，模式后移2位
+        if (patternIndex + 1 < pattern.length && pattern[patternIndex + 1] == '*') {
+            if ((strIndex != str.length && pattern[patternIndex] == str[strIndex]) || (pattern[patternIndex] == '.' && strIndex != str.length)) {
+                return matchCore(str, strIndex, pattern, patternIndex + 2)//模式后移2，视为x*匹配0个字符
+                        || matchCore(str, strIndex + 1, pattern, patternIndex + 2)//视为模式匹配1个字符
+                        || matchCore(str, strIndex + 1, pattern, patternIndex);//*匹配1个，再匹配str中的下一个
+            } else {
+                return matchCore(str, strIndex, pattern, patternIndex + 2);
             }
         }
-        return i == lStr && j == pStr;
+        //模式第2个不是*，且字符串第1个跟模式第1个匹配，则都后移1位，否则直接返回false
+        if ((strIndex != str.length && pattern[patternIndex] == str[strIndex]) || (pattern[patternIndex] == '.' && strIndex != str.length)) {
+            return matchCore(str, strIndex + 1, pattern, patternIndex + 1);
+        }
+        return false;
     }
 }
