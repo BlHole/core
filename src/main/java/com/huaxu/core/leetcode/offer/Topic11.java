c0b0ee4f9be97b222fb354930e3acff95fcfe173
huaxu <tshy0425@hotmail.com>
2020-02-21 16:18:35 +0800

    Arrow Offer Topic 11

--- src/main/java/com/huaxu/core/leetcode/offer/Topic11.java
+++ src/main/java/com/huaxu/core/leetcode/offer/Topic11.java
@@ -8,4 +8,16 @@ package com.huaxu.core.leetcode.offer;
  */
 public class Topic11 {
 
+    class Solution {
+        public int minArray(int[] numbers) {
+            int i = 0, j = numbers.length - 1;
+            while (i < j) {
+                int m = (i + j) / 2;
+                if (numbers[m] > numbers[j]) i = m + 1;
+                else if (numbers[m] < numbers[j]) j = m;
+                else j--;
+            }
+            return numbers[i];
+        }
+    }
 }
