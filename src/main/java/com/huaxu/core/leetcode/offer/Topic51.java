37f461cfc801c603c11bc17b35b2a8b234fa5222
huaxu <tshy0425@hotmail.com>
2020-02-28 18:24:31 +0800

    add part leetcode solution 051

---
+++ src/main/java/com/huaxu/core/leetcode/offer/Topic51.java
@@ -0,0 +1,29 @@
+package com.huaxu.core.leetcode.offer;
+
+/**
+ * <p>project: core</p>
+ * <p>description: </p>
+ * <p>create: 2020/02/28 18:08</p>
+ * <p>author：huaxu</p>
+ */
+public class Topic51 {
+
+    public static void main(String[] args) {
+        new Topic51().new Solution().singleNumbers(new int[]{1,2,3,4,1,4});
+    }
+
+    class Solution {
+        public int[] singleNumbers(int[] nums) {
+            int xor = 0;
+            for (int num : nums)
+                xor ^= num;
+            int diff = xor & (-xor);
+            int x = 0;
+            for (int num : nums) {
+                if ((diff & num) != 0)
+                    x ^= num;
+            }
+            return new int[]{x, xor ^ x};
+        }
+    }
+}
