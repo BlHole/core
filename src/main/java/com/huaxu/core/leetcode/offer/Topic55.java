b8e964a0d95380f1faf7f5ecdeceb6471fd38ae4
huaxu <tshy0425@hotmail.com>
2020-03-04 17:59:45 +0800

    Arrow Offer Topic 55

---
+++ src/main/java/com/huaxu/core/leetcode/offer/Topic55.java
@@ -0,0 +1,19 @@
+package com.huaxu.core.leetcode.offer;
+
+import com.huaxu.core.leetcode.basic.TreeNode;
+
+/**
+ * <p>project: core</p>
+ * <p>description: </p>
+ * <p>create: 2020/03/04 17:58</p>
+ * <p>author：huaxu</p>
+ */
+public class Topic55 {
+
+    class Solution {
+        public int maxDepth(TreeNode root) {
+            if (root == null) return 0;
+            return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
+        }
+    }
+}
